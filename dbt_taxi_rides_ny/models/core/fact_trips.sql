57afe082372dd70b40fe095ed5d89afdbffb3242
Mikita Tryzno <mikita_tryzno@epam.com>
2025-02-21 13:32:13 +0000

    dbt ny_taxi hw finalization

--- dbt_taxi_rides_ny/models/core/fact_trips.sql
+++ dbt_taxi_rides_ny/models/core/fact_trips.sql
@@ -5,25 +5,26 @@
 }}
 
 with green_tripdata as (
-    select *, 
-        'Green' as service_type
+    select *,
+    'Green' as service_type
     from {{ ref('stg_green_tripdata') }}
-), 
+),
 yellow_tripdata as (
-    select *, 
-        'Yellow' as service_type
+    select *,
+    'Yellow' as service_type
     from {{ ref('stg_yellow_tripdata') }}
 ),
 trips_unioned as (
     select * from green_tripdata
-    union all 
+    union all
     select * from yellow_tripdata
-), 
+),
 dim_zones as (
     select * from {{ ref('dim_zones') }}
     where borough != 'Unknown'
-)
-select trips_unioned.tripid, 
+),
+trips AS (
+    SELECT trips_unioned.tripid, 
     trips_unioned.vendorid, 
     trips_unioned.service_type,
     trips_unioned.ratecodeid, 
@@ -48,9 +49,17 @@ select trips_unioned.tripid,
     trips_unioned.improvement_surcharge, 
     trips_unioned.total_amount, 
     trips_unioned.payment_type, 
-    trips_unioned.payment_type_description
+    trips_unioned.payment_type_description,
+    EXTRACT(YEAR FROM trips_unioned.pickup_datetime) AS pickup_year,
+    EXTRACT(MONTH FROM trips_unioned.pickup_datetime) AS pickup_month,
+    EXTRACT(QUARTER FROM trips_unioned.pickup_datetime) AS pickup_quarter
 from trips_unioned
 inner join dim_zones as pickup_zone
 on trips_unioned.pickup_locationid = pickup_zone.locationid
 inner join dim_zones as dropoff_zone
 on trips_unioned.dropoff_locationid = dropoff_zone.locationid
+)
+
+SELECT trips.*,
+    CONCAT(CAST(pickup_year AS STRING), '/', CAST(pickup_quarter AS STRING)) AS year_quarter
+    from trips
